7e72f75e8d16d03b4c51d3b949fc09166cea8bcd
Baudino Sergio <bs105164@etu.unice.fr>
2014-04-17 05:21:12 -0700

    first commit

---
+++ TP6/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TP6::Application.config.secret_key_base = '3e02b6be6d342be719989e492e92028ec8d1c24ae26cd75f9e66b6a5b5413b40d0f62769b3ab9298522a03ac70e38c37bde8618da67ce8dc0539c3483b2941eb'
---
+++ TP6/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TP6::Application.config.session_store :cookie_store, key: '_TP6_session'
